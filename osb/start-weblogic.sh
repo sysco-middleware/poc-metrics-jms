4bb64c1e7ccf0a26666e06f131d83b59dcbf3405
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2017-06-19 23:39:23 +0200

    add osb container

--- osb/start-weblogic.sh
+++ osb/start-weblogic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export CURDIR="$(/bin/pwd)"
-export JAVA_OPTIONS="-javaagent:$CURDIR/jmx_prometheus_javaagent-0.9.jar=5555:$CURDIR/config.yml"
+export JAVA_OPTIONS="$JAVA_OPTIONS:-javaagent:$CURDIR/jmx_prometheus_javaagent-0.9.jar=5555:$CURDIR/config.yml"
 export OSB_DOMAIN="/opt/oracle/fmw/12.1.3.0.0/soaqs/user_projects/domains/osb_domain"
 sh "$OSB_DOMAIN"/startWebLogic.sh
